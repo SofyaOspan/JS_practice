b795d82c62c4bcfafa823775e86ae3d1d0bc7144
SonicX <ospanovasofya@gmail.com>
2023-08-09 16:59:20 +0600

    commit

---
+++ 8 Quote Generator/script.js
@@ -0,0 +1,29 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote')
+
+
+let apiQuotes = [];
+//show new quote
+function newQuote() {
+  // Pick a random quote from apiQuotes
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  authorText.textContent = quote.author;
+  quoteText.textContent = quote.text;
+}
+// Get Quotes from API
+async function getQuotes() {
+  const apiUrl = 'https://type.fit/api/quotes';
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    //alert(error)
+    //Catch Error Here
+  }
+}
+// On load
+getQuotes();
